7a729969acea4773b7351ead034d85ab556e67cc
shahid <kk.shahid66@gmail.com>
2020-07-17 23:23:19 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,81 @@
+function feetToMile(number){
+    let mile=number/5280;
+    let lessFractionNumber=mile.toFixed(3);
+    return lessFractionNumber;
+}
+
+function woodCalculator(chair,table,bed){
+let chairValue=chair*1;
+let tableValue=table*3;
+let bedValue=bed*5;
+let total=(chairValue+tableValue+bedValue);
+return total;
+}
+
+function birckCalculator(number){
+    let flore=number;
+    const bricks=1000;
+    let length;
+    let total;
+switch(number){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:    
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+
+         flore;
+         length=15;
+        
+         total=flore*length*bricks;
+        return total;
+    break;
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:    
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    case 20:
+
+        flore;
+        length=12;
+       
+        total=flore*length*bricks;
+       return total;
+    break;  
+    case 21:
+
+        flore;
+        length=10;
+       
+        total=flore*length*bricks;
+       return total;
+    break;
+
+    default:
+       return 0;
+
+
+}
+
+}
+
+
+
+
+let needToCalculate=woodCalculator(6,1,1);
+console.log("Your neeed ",needToCalculate,"cub fit wood")
+let distance=feetToMile(1000);
+console.log("Your calculate distance is", distance ," mile")
+let totalBricksNeed=birckCalculator(20);
+console.log("Your calculate bricks need ", totalBricksNeed )
+
